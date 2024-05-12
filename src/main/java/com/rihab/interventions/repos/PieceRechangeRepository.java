298f9ca18314c72e09eb447fe8062691f388c31f
Rihab <zouaouirihab03@gmail.com>
2024-05-12 21:13:32 +0100

    first commit

---
+++ src/main/java/com/rihab/interventions/repos/PieceRechangeRepository.java
@@ -0,0 +1,9 @@
+package com.rihab.interventions.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rihab.interventions.entities.PieceRechange;
+
+public interface PieceRechangeRepository extends JpaRepository<PieceRechange, Long> {
+
+}
---
+++ src/main/java/com/rihab/interventions/service/PieceRechangeService.java
@@ -0,0 +1,17 @@
+package com.rihab.interventions.service;
+
+import java.util.List;
+
+import com.rihab.interventions.entities.Technicien;
+
+public interface PieceRechangeService {
+	/*
+
+	PieceRechange savePieceRechange(Technicien technicien);
+	PieceRechange updateTechnicien(Technicien technicien);
+void deleteTechnicien(Technicien technicien);
+void deleteTechnicienByCode(long code);
+ Technicien getTechnicien(long code);
+List<Technicien> getAllTechniciens();
+*/
+}
